eae65461a3da10bc46630018436fe65c4cff871e
KodaiYM <solceansky@gmail.com>
2020-06-04 11:22:07 +0900

    add '+', '-'

--- compiler.cpp
+++ compiler.cpp
@@ -2,6 +2,7 @@
 #include <cstdlib>
 #include <iostream>
 #include <string>
+#include <vector>
 using namespace std::string_literals;
 
 int main(int argc, char *argv[]) {
@@ -10,17 +11,41 @@ int main(int argc, char *argv[]) {
 		return EXIT_FAILURE;
 	}
 
-	const std::string_view number = argv[1];
-	if (!std::all_of(number.begin(), number.end(), isdigit)) {
-		std::cerr << "compilation error!";
-		return EXIT_FAILURE;
-	}
+	std::string_view arg = argv[1];
 
 	std::cout << ".intel_syntax noprefix\n"
-	             ".global main\n"
-	             "\n"
-	             "main:\n"
-	          << "	mov rax, "s + argv[1] + "\n"
-	          << "	ret\n";
+	          << ".global main\n"
+	          << "\n"
+	          << "main:\n";
+
+	auto index = arg.find_first_not_of("0123456789");
+	if (index == arg.npos) {
+		index = arg.length();
+	}
+	// get first number and remove number from arg
+	auto firstNumber = arg.substr(0, index);
+	std::cout << "	mov rax, " << firstNumber << "\n";
+	arg.remove_prefix(index);
+
+	while (0 < arg.length()) {
+		switch (auto op = arg.front()) {
+		case '+':
+		case '-': {
+			arg.remove_prefix(1);
+			auto index = arg.find_first_not_of("0123456789");
+			if (arg.npos == index) {
+				index = arg.length();
+			}
+			auto number = arg.substr(0, index);
+			std::cout << "\t" << (op == '+' ? "add" : "sub") << " rax, " << number
+			          << "\n";
+			arg.remove_prefix(index);
+		} break;
+		default:
+			std::cerr << "invalid operator!" << std::endl;
+			std::exit(EXIT_FAILURE);
+		}
+	}
+	std::cout << "	ret\n";
 	return EXIT_SUCCESS;
 }
